67268d2b59813f53cbae96ffb4a5eb2ea0c1e8e3
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-08-08 15:01:31 -0400

    Cleanup

--- premake5.lua
+++ premake5.lua
@@ -37,24 +37,22 @@ project "glfw"
 		files
 		{
 			"src/cocoa_init.m",
-            "src/posix_module.c",
+			"src/posix_module.c",
 			"src/cocoa_joystick.m",
 			"src/cocoa_monitor.m",
 			"src/cocoa_time.h",
 			"src/cocoa_time.c",
 			"src/posix_time.c",
 			"src/posix_thread.h",
-            "src/posix_thread.c",
+			"src/posix_thread.c",
 			"src/cocoa_window.m",
-            "src/nsgl_context.m",
+			"src/nsgl_context.m",
 			"src/osmesa_context.c"
-
 		}
 
 		defines
 		{
-			"_GLFW_COCOA",
-			"GLFW_BUILD_COCOA"
+			"_GLFW_COCOA"
 		}
 
 	filter "system:linux"
